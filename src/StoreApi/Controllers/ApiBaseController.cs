6ecffd062dfa826d187b1844efa6e9f1f63af29f
srichatala <sri2vas@gmail.com>
2021-10-01 00:23:39 -0400

    Initial files added

---
+++ src/StoreApi/Controllers/ApiBaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+    }
+}
---
+++ src/StoreApi/Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApi.Controllers
+{
+    [Route("api/store")]
+    [ApiController]
+    public class StoreController : ApiBaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<string>> Get()
+        {
+            var list = new List<string>() { "Name", "LastName"};
+            return list;//await _mediator.Send(new GetAllCustomerQuery());
+        }
+    }
+}
